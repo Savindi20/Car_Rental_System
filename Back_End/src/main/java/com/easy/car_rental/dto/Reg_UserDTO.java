c3086e14c19d2dc4b51ead38dea4b312c57c8700
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-14 23:36:44 +0530

    reg_user entity dto classes added

---
+++ Back_End/src/main/java/com/easy/car_rental/dto/Reg_UserDTO.java
@@ -0,0 +1,15 @@
+package com.easy.car_rental.dto;
+
+public class Reg_UserDTO {
+    String user_Id;
+    String role_Type;
+    String name;
+    String contact_No;
+    String address;
+    String email;
+    String nic;
+    String license_No;
+    String password;
+    String nic_Img;
+    String license_Img;
+}
---
+++ Back_End/src/main/java/com/easy/car_rental/entity/Reg_User.java
@@ -0,0 +1,16 @@
+package com.easy.car_rental.entity;
+
+public class Reg_User {
+    String user_Id;
+    String role_Type;
+    String name;
+    String contact_No;
+    String address;
+    String email;
+    String nic;
+    String license_No;
+    String password;
+    String nic_Img;
+    String license_Img;
+
+}
