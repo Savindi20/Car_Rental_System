c7b409371264bb2b3a17ac6d9412bae7b3d9fe42
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-14 21:14:23 +0530

    scroll button update with js

--- Front_End/assets/js/main.js
+++ Front_End/assets/js/main.js
@@ -59,4 +59,60 @@ document.addEventListener('DOMContentLoaded', () => {
         mobileNavHide.classList.toggle('d-none');
     }
 
+    /* ================= Hide mobile nav on same-page/hash links ================= */
+    document.querySelectorAll('#navbar a').forEach(navbarlink => {
+
+        if (!navbarlink.hash) return;
+
+        let section = document.querySelector(navbarlink.hash);
+        if (!section) return;
+
+        navbarlink.addEventListener('click', () => {
+            if (document.querySelector('.mobile-nav-active')) {
+                mobileNavToogle();
+            }
+        });
+
+    });
+
+    /* ========================= Toggle mobile nav dropdowns ======================== */
+    const navDropdowns = document.querySelectorAll('.navbar .dropdown > a');
+
+    navDropdowns.forEach(el => {
+        el.addEventListener('click', function(event) {
+            if (document.querySelector('.mobile-nav-active')) {
+                event.preventDefault();
+                this.classList.toggle('active');
+                this.nextElementSibling.classList.toggle('dropdown-active');
+
+                let dropDownIndicator = this.querySelector('.dropdown-indicator');
+                dropDownIndicator.classList.toggle('bi-chevron-up');
+                dropDownIndicator.classList.toggle('bi-chevron-down');
+            }
+        })
+    });
+
+    /* ======================= Scroll top button ================== */
+    const scrollTop = document.querySelector('.scroll-top');
+    if (scrollTop) {
+        const togglescrollTop = function() {
+            window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+        }
+        window.addEventListener('load', togglescrollTop);
+        document.addEventListener('scroll', togglescrollTop);
+        scrollTop.addEventListener('click', window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        }));
+    }
+
+    /* ==================== Initiate glightbox ============*/
+    const glightbox = GLightbox({
+        selector: '.glightbox'
+    });
+
+    /* ================== Initiate pURE cOUNTER ===============*/
+    new PureCounter();
+
+
 });
