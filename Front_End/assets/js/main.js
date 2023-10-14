c180bc18b903c74ac8356097bf50e4910358924a
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-14 21:12:22 +0530

    sticky header update with js

---
+++ Front_End/assets/js/main.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+    "use strict";
+
+    /*  ========== Preloader ===============*/
+
+    const preloader = document.querySelector('#preloader');
+    if (preloader) {
+        window.addEventListener('load', () => {
+            preloader.remove();
+        });
+    }
+
+    /* ================= Sticky header on scroll =================*/
+
+    const selectHeader = document.querySelector('#header');
+    if (selectHeader) {
+        document.addEventListener('scroll', () => {
+            window.scrollY > 100 ? selectHeader.classList.add('sticked') : selectHeader.classList.remove('sticked');
+        });
+    }
+});
