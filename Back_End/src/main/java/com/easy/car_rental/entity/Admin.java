e9a5e311af533726a1abddab8d7f7aa85259ec9b
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-15 09:10:30 +0530

    admin entity class added

---
+++ Back_End/src/main/java/com/easy/car_rental/entity/Admin.java
@@ -0,0 +1,30 @@
+package com.easy.car_rental.entity;
+
+import com.easy.car_rental.embeded.Name;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Admin {
+    @Id
+    private String admin_Id;
+    private String role_Type;
+    @Embedded
+    private Name name;
+    private String contact_No;
+    private String address;
+    private String email;
+    private String nic;
+    private String password;
+
+}
