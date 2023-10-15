7c5b7dbaa5d337aee60ed77d258601bc8c78208b
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-15 08:50:44 +0530

    user controll file updated

--- Back_End/src/main/java/com/easy/car_rental/controller/Reg_UserController.java
+++ Back_End/src/main/java/com/easy/car_rental/controller/Reg_UserController.java
@@ -1,25 +1,24 @@
 package com.easy.car_rental.controller;
-
 import com.easy.car_rental.dto.Reg_UserDTO;
 import com.easy.car_rental.service.Reg_UserService;
 import com.easy.car_rental.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/reg_User")
 public class Reg_UserController {
-
     @Autowired
     private Reg_UserService service;
 
     @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping
-    public ResponseUtil saveCustomer(@ModelAttribute Reg_UserDTO dto) {
-
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil saveCustomer(@RequestBody Reg_UserDTO dto) {
         service.saveUser(dto);
+        System.out.println(dto);
         return new ResponseUtil("OK", "Successfully Registered.!", null);
     }
 
