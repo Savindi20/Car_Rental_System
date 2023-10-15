1dc8772f1a1c5492958b0acd9e437dd6c1a3aac9
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-15 09:19:15 +0530

    car entity class updated

--- Back_End/src/main/java/com/easy/car_rental/entity/Car.java
+++ Back_End/src/main/java/com/easy/car_rental/entity/Car.java
@@ -1,4 +1,37 @@
 package com.easy.car_rental.entity;
 
+import com.easy.car_rental.embeded.Image;
+import com.easy.car_rental.embeded.Rate;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Car {
+    @Id
+    private String car_Id;
+    private String name;
+    private String brand;
+    private String type;
+    @Embedded
+    private Image image;
+    private int number_Of_Passengers;
+    private String transmission_Type;
+    private String fuel_Type;
+    @Embedded
+    private Rate rent_Duration_Price;
+    private String price_Extra_KM;
+    private  String registration_Number;
+    private int car_qty;
+    private String color;
+
 }
