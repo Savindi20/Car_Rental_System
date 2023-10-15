bc8b72e9da1b65eff9aa841a0cfa8787af908dae
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-15 11:35:24 +0530

    driver controller save added

--- Back_End/src/main/java/com/easy/car_rental/controller/DriverController.java
+++ Back_End/src/main/java/com/easy/car_rental/controller/DriverController.java
@@ -1,12 +1,25 @@
 package com.easy.car_rental.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.easy.car_rental.dto.DriverDTO;
+import com.easy.car_rental.service.DriverService;
+import com.easy.car_rental.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/Driver")
 public class DriverController {
+    @Autowired
+    private DriverService service;
 
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil saveDriver(@RequestBody DriverDTO dto) {
+        service.saveDriver(dto);
+        System.out.println(dto);
+        return new ResponseUtil("OK", "Successfully Registered.!", null);
+    }
 }
