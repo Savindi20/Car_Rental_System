993a904612496faebab1103612c79cbf0ba3ddc5
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-15 09:34:50 +0530

    admin update add to the service class

--- Back_End/src/main/java/com/easy/car_rental/service/impl/AdminServiceImpl.java
+++ Back_End/src/main/java/com/easy/car_rental/service/impl/AdminServiceImpl.java
@@ -31,7 +31,10 @@ public class AdminServiceImpl implements AdminService {
 
     @Override
     public void updateAdmin(AdminDTO dto) {
-
+        if (!repo.existsById(dto.getAdmin_Id())) {
+            throw new RuntimeException("Admin Not Exist. Please enter Valid id..!");
+        }
+        repo.save(mapper.map(dto, Admin.class));
     }
 
     @Override
