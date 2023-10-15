69dd9a80a46437cc466d09ac1f168ced15669b4b
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-15 08:56:34 +0530

    content updated

--- Back_End/src/main/java/com/easy/car_rental/service/Reg_UserService.java
+++ Back_End/src/main/java/com/easy/car_rental/service/Reg_UserService.java
@@ -2,7 +2,12 @@ package com.easy.car_rental.service;
 
 import com.easy.car_rental.dto.Reg_UserDTO;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import com.easy.car_rental.dto.Reg_UserDTO;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.ArrayList;
 
 public interface Reg_UserService {
-    void saveUser(@ModelAttribute Reg_UserDTO dto);
+    void saveUser(Reg_UserDTO dto);
 }
