b2c5687665bfa6c6692e38b5ad7ce63a92e800b9
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-15 11:28:34 +0530

    Driver controller class added

---
+++ Back_End/src/main/java/com/easy/car_rental/controller/DriverController.java
@@ -0,0 +1,12 @@
+package com.easy.car_rental.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/Driver")
+public class DriverController {
+
+}
