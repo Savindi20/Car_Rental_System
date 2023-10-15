c4a40ba0e6bb33cb06cb8adb8e489d5a6bd5879f
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-15 12:09:40 +0530

    car controller save added

--- Back_End/src/main/java/com/easy/car_rental/controller/CarController.java
+++ Back_End/src/main/java/com/easy/car_rental/controller/CarController.java
@@ -1,7 +1,11 @@
 package com.easy.car_rental.controller;
 
+import com.easy.car_rental.dto.CarDTO;
 import com.easy.car_rental.service.CarService;
+import com.easy.car_rental.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,5 +15,13 @@ public class CarController {
 
     @Autowired
     private CarService service;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil saveCar(@RequestBody CarDTO dto) {
+        service.saveCar(dto);
+        System.out.println(dto);
+        return new ResponseUtil("OK", "Successfully Registered.!", null);
+    }
 
 }
