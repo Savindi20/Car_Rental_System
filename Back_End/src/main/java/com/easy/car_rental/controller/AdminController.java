f9e6415aba13033601d23e8246a176a0bb0ea9d7
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-15 09:44:57 +0530

    Admin controller class added

---
+++ Back_End/src/main/java/com/easy/car_rental/controller/AdminController.java
@@ -0,0 +1,18 @@
+package com.easy.car_rental.controller;
+
+import com.easy.car_rental.service.AdminService;
+import com.easy.car_rental.service.Reg_UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/Admin")
+public class AdminController {
+
+    @Autowired
+    private AdminService service;
+
+}
