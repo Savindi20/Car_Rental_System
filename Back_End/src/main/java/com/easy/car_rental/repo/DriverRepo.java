b06670e479c921090c4d41c91ffb0333a58484af
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-15 09:56:09 +0530

    driver repo class added

---
+++ Back_End/src/main/java/com/easy/car_rental/repo/DriverRepo.java
@@ -0,0 +1,10 @@
+package com.easy.car_rental.repo;
+
+import com.easy.car_rental.entity.Driver;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface DriverRepo extends JpaRepository<Driver, String> {
+}
