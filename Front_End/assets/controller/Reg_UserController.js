51b6c3e75be683974d78e6a13728cea377f0b078
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-16 13:17:08 +0530

    save and id generator added

---
+++ Front_End/assets/controller/Reg_UserController.js
@@ -0,0 +1,52 @@
+let userBaseUrl = "http://localhost:8083/Back_End_war/";
+loadAllRegUsers();
+$("#btnSaveCustomer").attr('disabled', true);
+$("#btnUpdateCustomer").attr('disabled', true);
+$("#btnDeleteCustomer").attr('disabled', true);
+
+/* ================ Customer Save ==============*/
+$("#btnSaveCustomer").click(function () {
+    let formData = new FormData($("#customerForm")[0]);
+    console.log(formData);
+    $.ajax({
+        url: userBaseUrl + "reg_User",
+        method: "post",
+        data: formData,
+        contentType: false,
+        processData: false,
+        success: function (res) {
+            saveUpdateAlert("User", res.message);
+            loadAllRegUsers();
+        },
+        error: function (error) {
+            unSuccessUpdateAlert("User", JSON.parse(error.responseText).message);
+        }
+    });
+});
+
+
+/* =========== User Id Generator ================*/
+function generateCustomerID() {
+    $("#user_Id").val("C00-001");
+    $.ajax({
+        url: userBaseUrl + "reg_User/reg_UserIdGenerate",
+        method: "GET",
+        contentType: "application/json",
+        dataType: "json",
+        success: function (resp) {
+            let id = resp.value;
+            console.log("id" + id);
+            let tempId = parseInt(id.split("-")[1]);
+            tempId = tempId + 1;
+            if (tempId <= 9) {
+                $("#user_Id").val("C00-00" + tempId);
+            } else if (tempId <= 99) {
+                $("#user_Id").val("C00-0" + tempId);
+            } else {
+                $("#user_Id").val("C00-" + tempId);
+            }
+        },
+        error: function (ob, statusText, error) {
+        }
+    });
+}
