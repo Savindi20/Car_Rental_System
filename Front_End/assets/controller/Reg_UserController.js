2ddb88b7bd30c026f3b8f60cc1e8ed0e3e880928
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-16 13:18:59 +0530

    load all method added

--- Front_End/assets/controller/Reg_UserController.js
+++ Front_End/assets/controller/Reg_UserController.js
@@ -69,3 +69,39 @@ function setTextFieldValues(firstName, lastName, contact_No, address, email, nic
     checkValidity(customerValidations);
     $("#btnSaveCustomer").attr('disabled', true);
 }
+
+/* =================== load all customers Method ===============*/
+function loadAllRegUsers() {
+    $("#customerTable").empty();
+    $.ajax({
+        url: userBaseUrl + "reg_User/loadAllUsers", method: "GET", dataType: "json", success: function (res) {
+            console.log(res);
+
+            for (let i of res.data) {
+                let user_Id = i.user_Id;
+                let firstName = i.name.firstName;
+                let lastName = i.name.lastName;
+                let contact_No = i.contact_No;
+                let address = i.address;
+                let email = i.email;
+                let nic = i.nic;
+                let license_No = i.license_No;
+                let nic_Img = i.nic_Img;
+                let license_Img = i.license_Img;
+                let role_Type = i.user.role_Type;
+                let user_Name = i.user.user_Name;
+                let password = i.user.password;
+
+                let row = "<tr><td>" + user_Id + "</td><td>" + firstName + "</td><td>" + lastName + "</td><td>" + contact_No + "</td><td>" + address + "</td><td>" + email + "</td><td>" + nic + "</td><td>" + license_No + "</td><td>" + role_Type + "</td><td>" + user_Name + "</td><td>" + password + "</td></tr>";
+                $("#customerTable").append(row);
+            }
+            blindClickEvents();
+            generateCustomerID();
+            setTextFieldValues("", "", "", "", "", "", "", "", "", "", "");
+            console.log(res.message);
+        }, error: function (error) {
+            let message = JSON.parse(error.responseText).message;
+            console.log(message);
+        }
+    });
+}
