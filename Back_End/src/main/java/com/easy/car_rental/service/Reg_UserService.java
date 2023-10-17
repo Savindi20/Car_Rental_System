f7bf414f7694f8d56efdd093f61df87b4060bc06
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-17 09:41:11 +0530

    customer dto added and id generater added to the service vlases

--- Back_End/src/main/java/com/easy/car_rental/service/Reg_UserService.java
+++ Back_End/src/main/java/com/easy/car_rental/service/Reg_UserService.java
@@ -1,5 +1,6 @@
 package com.easy.car_rental.service;
 
+import com.easy.car_rental.dto.CustomDTO;
 import com.easy.car_rental.dto.Reg_UserDTO;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import com.easy.car_rental.dto.Reg_UserDTO;
@@ -16,5 +17,7 @@ public interface Reg_UserService {
     void deleteUser(String reg_ID);
 
     ArrayList<Reg_UserDTO> getAllUser();
+
+    CustomDTO userIdGenerate();
 
 }
