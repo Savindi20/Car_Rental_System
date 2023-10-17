2335d54b9e3440ae05a1c4b2172dbb41e00f33e9
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-17 10:16:25 +0530

    exception handler added

--- Back_End/src/main/java/com/easy/car_rental/advisor/AppWideExceptionHandler.java
+++ Back_End/src/main/java/com/easy/car_rental/advisor/AppWideExceptionHandler.java
@@ -1,5 +1,19 @@
 package com.easy.car_rental.advisor;
 
-public class AppWideExceptionHandler {
+import com.easy.car_rental.util.ResponseUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@RestControllerAdvice
+@CrossOrigin
+public class AppWideExceptionHandler {
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler({RuntimeException.class})
+    public ResponseUtil handleMyExceptions(RuntimeException e) {
+        System.out.println(e);
+        return new ResponseUtil("Error", e.getMessage(), null);
+    }
 }
