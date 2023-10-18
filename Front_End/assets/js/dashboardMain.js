29433d4c549a3bc10b3a908a37446c5ee2d35f43
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-18 17:01:13 +0530

    back to button js

--- Front_End/assets/js/dashboardMain.js
+++ Front_End/assets/js/dashboardMain.js
@@ -130,4 +130,18 @@
             theme: "snow"
         });
     }
+
+    /* ========== Back to top button ========== */
+    let backtotop = select('.back-to-top')
+    if (backtotop) {
+        const toggleBacktotop = () => {
+            if (window.scrollY > 100) {
+                backtotop.classList.add('active')
+            } else {
+                backtotop.classList.remove('active')
+            }
+        }
+        window.addEventListener('load', toggleBacktotop)
+        onscroll(document, toggleBacktotop)
+    }
 })();
