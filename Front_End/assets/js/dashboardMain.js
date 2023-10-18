c9d5aae0feff5c1d794345a6604bcda125fa6bf3
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-18 16:56:37 +0530

    scroll js added

--- Front_End/assets/js/dashboardMain.js
+++ Front_End/assets/js/dashboardMain.js
@@ -49,4 +49,18 @@
     }
     window.addEventListener('load', navbarlinksActive)
     onscroll(document, navbarlinksActive)
+
+    /* ========== Toggle .header-scrolled class to #header when page is scrolled ========== */
+    let selectHeader = select('#header')
+    if (selectHeader) {
+        const headerScrolled = () => {
+            if (window.scrollY > 100) {
+                selectHeader.classList.add('header-scrolled')
+            } else {
+                selectHeader.classList.remove('header-scrolled')
+            }
+        }
+        window.addEventListener('load', headerScrolled)
+        onscroll(document, headerScrolled)
+    }
 })();
