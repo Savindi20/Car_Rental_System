94e542bc8a26431bfa1718a96682d65838b7a524
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-19 18:02:27 +0530

    searchBtn carSection rent list added

--- Front_End/controller/indexController.js
+++ Front_End/controller/indexController.js
@@ -94,6 +94,29 @@ function filterCarDetails() {
                 <div class="d-flex align-items-center col-lg-12 flex-wrap justify-content-center">
                     <img class="position-relative cars-img img-fluid m-3 p-2 col-lg-5" src="${carBaseUrl + url1}"
                          href="">
+
+                    <div class="position-relative col-xl-8 col-md-10 flex-wrap">
+
+                        <h4 class="m-2 p-2 text-center txt-head">${i.name + " " + i.brand} <br>
+                            <p><span>${i.type}</span><span>&nbsp;- &nbsp;</span><span>${i.transmission_Type}</span></p></h4>
+                        <div class="row listStyle m-1">
+                            <div class="col">
+                                <ul class="list-group">
+                                    <li class="list-group-item">Daily Rate (Rs) <strong>${i.rent_Duration_Price.daily_Rate}</strong></li>
+                                    <li class="list-group-item">Free <strong>${i.free_Mileage}</strong> KM for a Day</li>
+                                    <li class="list-group-item">Price per Extra KM (Rs) <strong>${i.price_Extra_KM}</strong>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="col">
+                                <ul class="list-group">
+                                    <li class="list-group-item">Monthly Rate (Rs) <strong>${i.rent_Duration_Price.monthly_Rate}</strong></li>
+                                    <li class="list-group-item">Free <strong>${(i.free_Mileage) * 24}</strong> KM for a Month</li>
+                                    <li class="list-group-item">Modification</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
  `);
