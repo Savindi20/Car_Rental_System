1e348a2b7db5134d0af926da83eac3b974889569
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-20 12:20:34 +0530

    search function added

--- Front_End/controller/regUserDashboardController.js
+++ Front_End/controller/regUserDashboardController.js
@@ -58,3 +58,41 @@ $("#fuel_Type").click(function () {
         }
     })
 });
+
+/* ========= Search ============ */
+$("#car_Id").click(function () {
+    var search = $("#car_Id").val();
+    $.ajax({
+        url: RentbaseUrl + "car/searchCar/?car_Id=" + search,
+        method: "GET",
+        contentType: "application/json",
+        dataType: "json",
+        success: function (res) {
+            console.log(res);
+            $("#name").val(res.name);
+            $("#brand").val(res.brand);
+            $("#number_Of_Passengers").val(res.number_Of_Passengers);
+            let url1 = res.image.front_View;
+            let url2 = res.image.back_View;
+            let url3 = res.image.side_View;
+            let url4 = res.image.interior;
+            $("#imageLoad1").css({
+                "background": `url(${RentbaseUrl + url1})`, "background-size": "cover"
+            });
+            $("#imageLoad2").css({
+                "background": `url(${RentbaseUrl + url2})`, "background-size": "cover"
+            });
+            $("#imageLoad3").css({
+                "background": `url(${RentbaseUrl + url3})`, "background-size": "cover"
+            });
+            $("#imageLoad4").css({
+                "background": `url(${RentbaseUrl + url4})`, "background-size": "cover"
+            });
+        },
+        error: function (error) {
+            let message = JSON.parse(error.responseText).message;
+            console.log(message);
+        }
+    })
+});
+
