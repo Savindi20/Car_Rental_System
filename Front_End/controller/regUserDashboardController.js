56ddea1104f91756f238615ff002a9afe8c61dad
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-20 12:18:46 +0530

    id generator function  added

--- Front_End/controller/regUserDashboardController.js
+++ Front_End/controller/regUserDashboardController.js
@@ -2,3 +2,31 @@ let RentbaseUrl = "http://localhost:8083/Back_End_war/";
 
 loadAllRent();
 $("#updateCustomer").attr('disabled', true);
+
+/* ========= User Id Generator ============ */
+generateRentID();
+
+function generateRentID() {
+    $("#rent_Id").val("REN-001");
+    $.ajax({
+        url: RentbaseUrl + "rent/rentIdGenerate",
+        method: "GET",
+        contentType: "application/json",
+        dataType: "json",
+        success: function (resp) {
+            let id = resp.value;
+            console.log("id" + id);
+            let tempId = parseInt(id.split("-")[1]);
+            tempId = tempId + 1;
+            if (tempId <= 9) {
+                $("#rent_Id").val("REN-00" + tempId);
+            } else if (tempId <= 99) {
+                $("#rent_Id").val("REN-0" + tempId);
+            } else {
+                $("#rent_Id").val("REN-" + tempId);
+            }
+        },
+        error: function (ob, statusText, error) {
+        }
+    });
+}
