88299a49e58915a76029b0dd38a6ae6e7f0ec17c
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-20 12:28:35 +0530

    table load added

--- Front_End/controller/regUserDashboardController.js
+++ Front_End/controller/regUserDashboardController.js
@@ -150,3 +150,20 @@ $("#btnAddCart").on("click", function () {
 
     });
 });
+
+/* ============ Place order, Table Load =========== */
+$("#cartTable").empty();
+
+function loadCartTableDetail() {
+    car_Id = $("#car_Id").val();
+    pickUpDate = $("#pickUpDate").val();
+    pickUpTime = $("#pickUpTime").val();
+    returnDate = $("#returnDate").val();
+    returnTime = $("#returnTime").val();
+    requestType = $("#requestType").val();
+    goLocation = $("#location").val();
+
+    let row = `<tr><td>${car_Id}</td><td>${pickUpDate}</td><td>${pickUpTime}</td><td>${returnDate}</td><td>${returnTime}</td><td>${requestType}</td><td>${goLocation}</td></tr>`;
+
+    $("#cartTable").append(row);
+}
