4edf7fa7983aae90292ea2cae09700224d51a24f
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-21 16:25:34 +0530

    All Rent Details updated

--- Front_End/controller/RentController.js
+++ Front_End/controller/RentController.js
@@ -11,5 +11,14 @@ function loadAllRentDetails() {
         contentType: "application/json",
         dataType: "json",
         async: true,
+        success: function (res) {
+            console.log(res.data)
+            for (var i of res.data) {
+                let row = "<tr><td>" + i.rentID + "</td><td>" + i.rentDetails.at().carID + "</td><td>" + i.regUser.user_Id + "</td><td>" + i.rentDetails.at().driverID + "</td><td>" + i.requestType + "</td><td>" + i.rentType + "</td><td>" + i.pickUpDate + "</td><td>" + i.pickUpTime + "</td><td>" + i.returnTime + "</td><td>" + i.returnDate + "</td><td>" + i.location + "</td></tr>";
+                $("#rentAllDetails").append(row);
+                $("#retManage").append(row);
+                blindClickEventsRent();
+            }
+        }
     });
 }
