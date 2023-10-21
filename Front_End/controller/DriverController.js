c9586abeeae659ae83e596763a0045a5d0732add
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-21 12:53:42 +0530

    Customer Save added

--- Front_End/controller/DriverController.js
+++ Front_End/controller/DriverController.js
@@ -4,3 +4,25 @@ loadAllDrivers();
 $("#btnSaveDriver").attr('disabled', true);
 $("#btnUpdateDriver").attr('disabled', true);
 $("#btnDeleteDriver").attr('disabled', true);
+
+
+/* =========== Customer Save ============= */
+$("#btnSaveDriver").click(function () {
+    let formData = new FormData($("#driverForm")[0]);
+    console.log(formData);
+    $.ajax({
+        url: driverBaseUrl + "driver",
+        method: "post",
+        data: formData,
+        contentType: false,
+        processData: false,
+        success: function (res) {
+            console.log(res)
+            saveUpdateAlert("Driver", res.message);
+            loadAllDrivers();
+        },
+        error: function (error) {
+            unSuccessUpdateAlert("Driver", JSON.parse(error.responseText).message);
+        }
+    });
+});
