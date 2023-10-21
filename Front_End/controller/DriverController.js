9caa74588ed145fc4732fb612cf570cc1f6d6297
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-21 12:53:13 +0530

    controller basic added

---
+++ Front_End/controller/DriverController.js
@@ -0,0 +1,6 @@
+let driverBaseUrl = "http://localhost:8083/Back_End_war/";
+loadAllDrivers();
+
+$("#btnSaveDriver").attr('disabled', true);
+$("#btnUpdateDriver").attr('disabled', true);
+$("#btnDeleteDriver").attr('disabled', true);
