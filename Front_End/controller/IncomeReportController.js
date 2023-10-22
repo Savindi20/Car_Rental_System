94d9e0523ee172a6dafa82338da0ef826f0c0e7a
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-22 11:55:09 +0530

    Daily Income added

--- Front_End/controller/IncomeReportController.js
+++ Front_End/controller/IncomeReportController.js
@@ -2,4 +2,17 @@ let baseUrlIncome = "http://localhost:8083/Back_End_war/";
 
 document.addEventListener("DOMContentLoaded", () => {
 
+    /* ======== Daily Income ========== */
+    let dataPoints1 = [];
+    let options1 = {
+        animationEnabled: true, theme: "light2", title: {
+            text: "Daily Sales Income"
+        }, axisX: {
+            valueFormatString: "YYYY MMM DD",
+        }, axisY: {
+            title: "LKR", titleFontSize: 20
+        }, data: [{
+            type: "spline", yValueFormatString: "$#,###.##", dataPoints: dataPoints1
+        }]
+    };
 });
