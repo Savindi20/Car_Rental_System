6d33738b5b30e85ffb6c9d76bb8f40d159e5914c
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-24 15:29:02 +0530

    getAllRegUser method added

--- Back_End/src/main/java/com/easy/car_rental/controller/LoginController.java
+++ Back_End/src/main/java/com/easy/car_rental/controller/LoginController.java
@@ -1,7 +1,11 @@
 package com.easy.car_rental.controller;
 
 import com.easy.car_rental.service.UserService;
+import com.easy.car_rental.util.CurrentUser;
+import com.easy.car_rental.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -10,5 +14,12 @@ import org.springframework.web.bind.annotation.*;
 public class LoginController {
     @Autowired
     private UserService service;
+
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil getAllRegUser(){
+        System.out.println(service.getAllRegUsers());
+        return new ResponseUtil("OK","Successfully Loaded..!",service.getAllRegUsers());
+    }
 
 }
