98ed47f63caf18c44d1c74d810acea34ca9bdadd
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-25 16:28:57 +0530

    getAllRents searchId method added

--- Back_End/src/main/java/com/easy/car_rental/controller/RentController.java
+++ Back_End/src/main/java/com/easy/car_rental/controller/RentController.java
@@ -1,7 +1,11 @@
 package com.easy.car_rental.controller;
 
+import com.easy.car_rental.dto.CustomDTO;
+import com.easy.car_rental.dto.RentDTO;
 import com.easy.car_rental.service.RentService;
+import com.easy.car_rental.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -10,4 +14,18 @@ import org.springframework.web.bind.annotation.*;
 public class RentController {
     @Autowired
     private RentService service;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @GetMapping(path = "/loadAllRents")
+    public ResponseUtil getAllRents() {
+        System.out.println(service.getAllRent());
+        return new ResponseUtil("OK", "Successfully Loaded. :", service.getAllRent());
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @GetMapping(path = "/searchDtails", params = {"search_Id"})
+    public RentDTO searchId(String search_Id) {
+        return service.searchId(search_Id);
+    }
+
 }
