5a5c29d34d47764d2a107d937252e17a4c71729f
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-25 12:28:17 +0530

    getAllRent searchId methods updated

--- Back_End/src/main/java/com/easy/car_rental/service/RentService.java
+++ Back_End/src/main/java/com/easy/car_rental/service/RentService.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
 
 public interface RentService {
 
+    CustomDTO rentIdGenerate();
+
     void bookingCars(@RequestBody RentDTO dto);
 
     ArrayList <RentDTO> getAllRent();
--- Back_End/src/main/java/com/easy/car_rental/service/impl/RentServiceImpl.java
+++ Back_End/src/main/java/com/easy/car_rental/service/impl/RentServiceImpl.java
@@ -1,7 +1,14 @@
 package com.easy.car_rental.service.impl;
 
+import com.easy.car_rental.dto.CustomDTO;
 import com.easy.car_rental.dto.RentDTO;
+import com.easy.car_rental.repo.CarRepo;
+import com.easy.car_rental.repo.DriverRepo;
+import com.easy.car_rental.repo.RentRepo;
 import com.easy.car_rental.service.RentService;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,6 +17,19 @@ import java.util.ArrayList;
 @Service
 @Transactional
 public class RentServiceImpl implements RentService {
+    @Autowired
+    private RentRepo rentRepo;
+    @Autowired
+    private CarRepo carRepo;
+    @Autowired
+    private DriverRepo driverRepo;
+    @Autowired
+    private ModelMapper mapper;
+
+    @Override
+    public CustomDTO rentIdGenerate() {
+        return null;
+    }
 
     @Override
     public void bookingCars(RentDTO dto) {
@@ -18,7 +38,8 @@ public class RentServiceImpl implements RentService {
 
     @Override
     public ArrayList<RentDTO> getAllRent() {
-        return null;
+        return mapper.map(rentRepo.findAll(), new TypeToken<ArrayList<RentDTO>>() {
+        }.getType());
     }
 
     @Override
@@ -28,6 +49,10 @@ public class RentServiceImpl implements RentService {
 
     @Override
     public RentDTO searchId(String id) {
-        return null;
+        if (!rentRepo.existsById(id)) {
+            throw new RuntimeException("Wrong ID. Please enter Valid id..!");
+        }
+        return mapper.map(rentRepo.findById(id).get(), RentDTO.class);
+
     }
 }
