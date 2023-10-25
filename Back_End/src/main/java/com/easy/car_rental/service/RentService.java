f38d973a99151dee139397d6524f34df74f44888
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-25 12:06:11 +0530

    RentServiceImpl methods added

--- Back_End/src/main/java/com/easy/car_rental/service/RentService.java
+++ Back_End/src/main/java/com/easy/car_rental/service/RentService.java
@@ -1,4 +1,18 @@
 package com.easy.car_rental.service;
 
+import com.easy.car_rental.dto.CustomDTO;
+import com.easy.car_rental.dto.RentDTO;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.ArrayList;
+
 public interface RentService {
+
+    void bookingCars(@RequestBody RentDTO dto);
+
+    ArrayList <RentDTO> getAllRent();
+
+    void deleteRent(String rentID);
+
+    RentDTO searchId(String id);
 }
--- Back_End/src/main/java/com/easy/car_rental/service/impl/RentServiceImpl.java
+++ Back_End/src/main/java/com/easy/car_rental/service/impl/RentServiceImpl.java
@@ -1,11 +1,33 @@
 package com.easy.car_rental.service.impl;
 
+import com.easy.car_rental.dto.RentDTO;
 import com.easy.car_rental.service.RentService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
 
 @Service
 @Transactional
 public class RentServiceImpl implements RentService {
 
+    @Override
+    public void bookingCars(RentDTO dto) {
+
+    }
+
+    @Override
+    public ArrayList<RentDTO> getAllRent() {
+        return null;
+    }
+
+    @Override
+    public void deleteRent(String rentID) {
+
+    }
+
+    @Override
+    public RentDTO searchId(String id) {
+        return null;
+    }
 }
