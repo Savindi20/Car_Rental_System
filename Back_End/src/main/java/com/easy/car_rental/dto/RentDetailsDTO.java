f9c4461572f00328c32f2bf41f18031db34cb5b5
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-25 11:11:16 +0530

    RentDetailsDTO class added

---
+++ Back_End/src/main/java/com/easy/car_rental/dto/RentDetailsDTO.java
@@ -0,0 +1,17 @@
+package com.easy.car_rental.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Data
+
+public class RentDetailsDTO {
+    private String carID;
+    private String rentID;
+    private String driverID;
+}
