68c63e1a6b2c28772e138179fde5ff3cc566cf44
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-26 20:27:29 +0530

    IncomeController class added

---
+++ Back_End/src/main/java/com/easy/car_rental/controller/IncomeController.java
@@ -0,0 +1,9 @@
+package com.easy.car_rental.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/income")
+public class IncomeController {
+}
