aa4b4b333b0422f63114c8d77fbbc1f12335ed28
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-26 17:14:50 +0530

    savePayment method added

--- Back_End/src/main/java/com/easy/car_rental/service/impl/PaymentServiceImpl.java
+++ Back_End/src/main/java/com/easy/car_rental/service/impl/PaymentServiceImpl.java
@@ -39,8 +39,31 @@ public class PaymentServiceImpl implements PaymentService {
     private ModelMapper mapper;
 
     @Override
-    public void savePayment(PaymentDTO dto, String rentID) {
+    public void savePayment(PaymentDTO dto,String rentID) {
+        Payment payment = mapper.map(dto, Payment.class);
+        Rent rent = rentRepo.findById(rentID).get();
+        if (rent.getRentDetails().get(0).getDriverID() != null) {
 
+            Driver drivers = driverRepo.findById(rent.getRentDetails().get(0).getDriverID()).get();
+            drivers.setDriverAvailability(AVAILABLE);
+            driverRepo.save(drivers);
+
+            Car car = carRepo.findById(rent.getRentDetails().get(0).getCarID()).get();
+            car.setVehicleAvailabilityType(UNDER_MAINTAIN);
+            carRepo.save(car);
+
+            rent.setRentType(PAY);
+            rentRepo.save(rent);
+        }
+        if (rent.getRentDetails().get(0).getDriverID() == null) {
+            Car car = carRepo.findById(rent.getRentDetails().get(0).getCarID()).get();
+            car.setVehicleAvailabilityType(UNDER_MAINTAIN);
+            carRepo.save(car);
+
+            rent.setRentType(PAY);
+            rentRepo.save(rent);
+        }
+        paymentRepo.save(payment);
     }
 
     @Override
