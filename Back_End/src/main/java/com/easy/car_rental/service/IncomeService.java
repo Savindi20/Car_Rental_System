de4d8c58900c0279c15f55686f9d7db93d93ca32
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-26 20:25:37 +0530

    IncomeService classes added

---
+++ Back_End/src/main/java/com/easy/car_rental/service/IncomeService.java
@@ -0,0 +1,4 @@
+package com.easy.car_rental.service;
+
+public interface IncomeService {
+}
---
+++ Back_End/src/main/java/com/easy/car_rental/service/impl/IncomeServiceImpl.java
@@ -0,0 +1,10 @@
+package com.easy.car_rental.service.impl;
+
+import com.easy.car_rental.service.IncomeService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class IncomeServiceImpl implements IncomeService {
+}
