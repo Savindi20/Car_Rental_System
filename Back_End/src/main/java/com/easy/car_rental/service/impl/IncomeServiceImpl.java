7cf3cfa42655662348d71d914b48f7e2bcbe953b
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-27 09:41:54 +0530

    IncomeService methoods added

--- Back_End/src/main/java/com/easy/car_rental/service/impl/IncomeServiceImpl.java
+++ Back_End/src/main/java/com/easy/car_rental/service/impl/IncomeServiceImpl.java
@@ -1,10 +1,27 @@
 package com.easy.car_rental.service.impl;
 
+import com.easy.car_rental.dto.IncomeDTO;
 import com.easy.car_rental.service.IncomeService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+
 @Service
 @Transactional
 public class IncomeServiceImpl implements IncomeService {
+    @Override
+    public ArrayList<IncomeDTO> dailyIncome() {
+        return null;
+    }
+
+    @Override
+    public ArrayList<IncomeDTO> monthlyIncome() {
+        return null;
+    }
+
+    @Override
+    public ArrayList<IncomeDTO> AnnuallyIncome() {
+        return null;
+    }
 }
