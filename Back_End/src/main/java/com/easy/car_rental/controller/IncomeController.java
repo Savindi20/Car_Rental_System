dd26f61f11f9215dbf4ba1365e82eb3560a85d77
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-27 10:34:20 +0530

    dailyIncome controller method added

--- Back_End/src/main/java/com/easy/car_rental/controller/IncomeController.java
+++ Back_End/src/main/java/com/easy/car_rental/controller/IncomeController.java
@@ -1,8 +1,12 @@
 package com.easy.car_rental.controller;
 
+import com.easy.car_rental.dto.IncomeDTO;
 import com.easy.car_rental.service.IncomeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @CrossOrigin
@@ -11,4 +15,9 @@ public class IncomeController {
     @Autowired
     private IncomeService service;
 
+    @ResponseStatus(HttpStatus.CREATED)
+    @GetMapping(path = "/dailyIncome")
+    public @ResponseBody ArrayList<IncomeDTO> dailyIncome() {
+        return service.dailyIncome();
+    }
 }
