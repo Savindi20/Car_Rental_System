f79f0f2e0e7e61e5b2fa90c2b2731d4380420e42
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-27 11:14:35 +0530

    MonthlyIncome query method added

--- Back_End/src/main/java/com/easy/car_rental/repo/IncomeRepo.java
+++ Back_End/src/main/java/com/easy/car_rental/repo/IncomeRepo.java
@@ -7,7 +7,9 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.ArrayList;
 
 public interface IncomeRepo extends JpaRepository<Rent, String> {
-
     @Query(value = "SELECT payment.date,count(payment.paymentID),sum(payment.total) FROM Rent INNER JOIN Payment ON Rent.rentID = Payment.rentID GROUP BY date", nativeQuery = true)
     ArrayList dailyIncome();
+
+    @Query(value = "SELECT (MONTHNAME(date )) ,count(payment.paymentID),sum(payment.total)FROM Rent INNER JOIN Payment ON Rent.rentID = Payment.rentID  GROUP BY extract(MONTH FROM(date))", nativeQuery = true)
+    ArrayList MonthlyIncome();
 }
